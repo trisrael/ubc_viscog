1bc1071451760375afdd93e24677563ae11bb95c
Tristan Goffman <tgoffman@gmail.com>
2011-07-20 15:07:40 -0700

    add in interaction architecture to modularize out the process of user.. etc interactions from experiments

---
+++ src/StevensLaw/GraphInteraction.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package StevensLaw;
+
+import interaction.ExperimentInteraction;
+
+public enum GraphInteraction implements ExperimentInteraction{
+    CorrelationUp,
+    CorrelationDown
+}
+
